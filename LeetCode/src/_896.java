23593baa811ff2a0b6a12bfaa6a8a51a98121c58
mohamedelkashif <mohamed_elkashif@windowslive.com>
2020-01-06 15:35:38 +0200

    Another efficient solution

--- LeetCode/src/_896.java
+++ LeetCode/src/_896.java
@@ -22,3 +22,23 @@ public class _896 {
 
     }
 }
+
+// Another efficient solution
+
+class Solution {
+    public boolean isMonotonic(int[] A) {
+        return increasing(A) || decreasing(A);
+    }
+
+    public boolean increasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] > A[i+1]) return false;
+        return true;
+    }
+
+    public boolean decreasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] < A[i+1]) return false;
+        return true;
+    }
+}
