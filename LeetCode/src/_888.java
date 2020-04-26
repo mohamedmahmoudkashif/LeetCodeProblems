411b454047f9cd67e3d5f4eba52969fd2d877baf
mohamedelkashif <mohamed_elkashif@windowslive.com>
2020-04-26 17:59:00 +0200

    last problem

--- LeetCode/src/_888.java
+++ LeetCode/src/_888.java
@@ -4,19 +4,6 @@ import java.util.List;
 
 public class _888 {
     public static boolean isPrime(int num) {
-//        Arrays.sort(A);
-//        Arrays.sort(B);
-//
-//        int a = A[A.length-1];
-//        int b = B[B.length-1];
-//
-//        int [] ans = new int [2];
-//        ans[0] = a;
-//        ans[1] = b;
-//        for(int i=0;i<ans.length;i++){
-//            System.out.println(ans[i]);
-//        }
-//        return ans;
 //        if(B <= 1){
 //            return false;
 //        }
