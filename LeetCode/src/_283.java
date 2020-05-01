a09a537c393793c442385b2e68a273bf59dcb5e9
mohamedelkashif <mohamed_elkashif@windowslive.com>
2020-05-01 02:10:48 +0200

    Fast solution for 283 problem

--- LeetCode/src/_283.java
+++ LeetCode/src/_283.java
@@ -16,4 +16,20 @@ class Solution {
            }
        }
     }
+
+    /** Fast performant solution  */
+    public void moveZeroes(int [] nums){
+        int pos = 0;
+        for (int i=0;i<nums.length;i++){
+            if(nums[i] !=0 ){
+                if(i != pos){
+                    int temp = nums[i];
+                    nums[i] = nums[pos];
+                    nums[pos] = temp;
+                }
+                pos++;
+
+            }
+        }
+    }
 }
